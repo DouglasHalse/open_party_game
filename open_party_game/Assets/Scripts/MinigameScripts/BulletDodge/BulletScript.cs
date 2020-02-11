bb1e6d5c39f8703b7f7ca7b6c64a014f18a9e0f9
Douglas Halse <4headmaster@gmail.com>
2020-02-11 21:23:52 +0100

    Added functioning turret prefab for BulletDodge minigame

---
+++ open_party_game/Assets/Scripts/MinigameScripts/BulletDodge/BulletScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public GameObject target;
+    private float speed = 10f;
+    private float rotating_speed = 20f;
+    private float self_destruct_time = 5f;
+    public void setup_and_shoot(GameObject target, float speed, float rotating_speed, float self_destruct_timer)
+    {
+        this.target = target;
+        this.speed = speed;
+        this.rotating_speed = rotating_speed;
+        this.self_destruct_time = self_destruct_timer;
+        this.enabled = true;
+    }
+    public void set_target(GameObject target)
+    {
+        this.target = target;
+    }
+    public void set_speed(int speed)
+    {
+        this.speed = speed;
+    }
+    public void set_rotating_speed(int rotating_speed)
+    {
+        this.rotating_speed = rotating_speed;
+    }
+    private Quaternion look_at_target()
+    {
+        Vector3 direction = (target.transform.position - this.transform.position).normalized;
+        Quaternion rot = Quaternion.LookRotation(direction, Vector3.up);
+        rot.SetEulerAngles(90*Mathf.Deg2Rad, Quaternion.LookRotation(direction, Vector3.up).eulerAngles.y * Mathf.Deg2Rad, 0);
+        return rot;
+    }
+    private void move_forward()
+    {
+        Vector3 frw_dir = this.gameObject.transform.up.normalized;
+        this.transform.position += frw_dir * Time.deltaTime * speed;
+    }
+    private void rotate_toward_target()
+    {
+        this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, look_at_target(), rotating_speed * Time.deltaTime);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, self_destruct_time);
+        //Destroy(this, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move_forward();
+        rotate_toward_target();
+    }
+}
---
+++ open_party_game/Assets/Scripts/MinigameScripts/BulletDodge/TurretScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] players;
+    public GameObject projectile;
+    private GameObject rotater;
+    private GameObject barrel;
+    private GameObject current_target;
+    
+    public GameObject get_current_target()
+    {
+        return current_target;
+    }
+    private Quaternion get_direction()
+    {
+        Vector3 player_pos = current_target.transform.position;
+        player_pos.y = 0;
+        Quaternion dir = Quaternion.LookRotation(player_pos, Vector3.up);
+        
+        return dir;
+    }
+    public void set_target(GameObject target)
+    {
+        this.current_target = target;
+    }
+
+    public void fire_turret()
+    {
+        //Vector3 fire_dir = (current_target.transform.position - barrel.transform.position).normalized;
+        Vector3 direction = barrel.transform.up;
+        Quaternion rot = Quaternion.LookRotation(direction, Vector3.up);
+        rot.SetEulerAngles(90 * Mathf.Deg2Rad, Quaternion.LookRotation(direction, Vector3.up).eulerAngles.y * Mathf.Deg2Rad, 0);
+        GameObject bullet = Instantiate(projectile, barrel.transform.position + barrel.transform.up, rot);
+        bullet.GetComponent<BulletScript>().setup_and_shoot(current_target, 10f, 20f, 5f);
+
+    }
+    void Start()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        rotater = this.gameObject.transform.GetChild(0).GetChild(0).gameObject;
+        barrel = this.gameObject.transform.GetChild(0).GetChild(0).GetChild(0).gameObject;
+        current_target = players[0];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            fire_turret();
+        }
+        rotater.transform.rotation = get_direction();
+    }
+}
